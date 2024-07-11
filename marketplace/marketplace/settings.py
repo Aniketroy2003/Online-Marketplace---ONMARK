64a80b969a600fb72f2ea3f74a54c27b3cc1f293
Aniket Roy <spacelover2003@gmail.com>
2024-07-12 01:25:22 +0530

    new

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-1ur_$#6#o=dsk3fp_o5%sp-)%dhq8!e4xgg!_*nx1n4&q6f3oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','online-marketplace-onmark-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://online-marketplace-onmark-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
+# CSRF_TRUSTED_ORIGINS = ['*']
 
 
 LOGIN_URL = '/login/'
